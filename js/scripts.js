c2f660e0646ab364061944c838c20ca6bab811b1
Stephen McDaniel and Stephanie Wills <stephanie@>
2019-02-11 20:45:28 -0800

    worst math machine ever

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".calc-btn-1").click(function() {
+    alert("WORKED!");
+    });
+
+  var OneInput = $("input#number1").val();
+  console.log(OneInput + " this worked!");
+
+  });
